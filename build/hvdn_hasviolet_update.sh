3730e8883825cb79e6f6853da59f479d792f5f00
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2019-09-15 02:52:45 -0400

    Shell script updates

--- build/hvdn_hasviolet_update.sh
+++ build/hvdn_hasviolet_update.sh
@@ -28,7 +28,7 @@
 ##
 
 # HVDN Archive Filename
-hvdn_hasviolet_archive="hvdn-comm_"+ ${CURRENTEPOCTIME}
+hvdn_hasviolet_archive="hvdn-comm_${CURRENTEPOCTIME}"
 
 # HVDN LocalRepo Home Directory
 hvdn_localrepo=$HOME/hvdn
