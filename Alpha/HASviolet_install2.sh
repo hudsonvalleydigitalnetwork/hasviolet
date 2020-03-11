f3b10a44832b8997767cac2e78f8d960ccf5e66c
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2020-03-11 06:23:17 -0400

    Update instll script

--- Alpha/HASviolet_install2.sh
+++ Alpha/HASviolet_install2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###
-### HVDN_HASVIOLET_INSTALL
+### HASVIOLET_INSTALL
 ###
 
 ##
@@ -24,7 +24,7 @@
 ## 2019-09-14	Includes pyRF95 repo
 ## 2020-02-12   Permission error for MOTD, Changing working app path, Clean up script
 ## 2020-03-03   Rebrand
-## 2020-03-07   Missing clone command 
+## 2020-03-07   Missing git clone command -- duh
 ## 2020-03-11   Missing mkdir command
 
 ##
@@ -75,7 +75,7 @@ sudo pip3 install adafruit-circuitpython-framebuf
 
 echo " "
 echo "- Clone HASviolet Repository"
-echo " "
+echi " "
 
 mkdir $hvdn_localrepo
 cd $hvdn_localrepo
@@ -92,5 +92,8 @@ echo " "
 echo "HASviolet installation complete."
 echo "All apps are in $hasviolet_install"
 echo " "
+echo "To run the apps, you must be in the $hasviolet_install directory and"
+echo "prefix the app name with .\/\<app-name\>"
 
+cd $hasviolet_install
 exit 0
